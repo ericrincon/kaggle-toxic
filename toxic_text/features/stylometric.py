8ec10e761b39e6c28c2c27de57db693fd51f09cf
ericrincon <eric.rincon147@gmail.com>
2018-03-09 00:01:24 -0600

    Method for getting the ratio of uppercase to total characters. Fixed docstrings  a bit

--- toxic_text/features/stylometric.py
+++ toxic_text/features/stylometric.py
@@ -8,13 +8,17 @@ class CharacterFeatures:
         """
         self.exclamation_point_count = re.compile('\!')
         self.char_count = re.compile('[^\s]]')
+        self.upper_case = re.compile('[A-Z]]')
+
+
 
     def get_nb_exclamation_points(self, text):
         """
         Returns the number of "!" in a comment
 
-        :param text:
-        :return:
+        :param text: a pandas series of strings
+        :return: pandas series where each element is the numebr of exclamation
+        points in that element
         """
         return text.apply(lambda x: len(self.exclamation_point_count.findall(x)))
 
@@ -22,7 +26,22 @@ class CharacterFeatures:
         """
         Returns the number of characters in a string excluding whitespace
         :param text: a pandas series of strings
-        :return: number of characters in each string
+        :return: pandas series where each element is
+        the number of characters in that string
         """
         return text.apply(lambda x: len(self.char_count.findall(x)))
+
+    def upper_case_ratio(self, text):
+        """
+        Get the number of upper case characters over the total number
+        of characters excluding whitespace
+
+        :return: pandas series where each element is
+        the ratio of uppercase over total counts
+        """
+
+        char_counts = self.get_char_count(text)
+        upper_case_counts = text.apply(lambda x: len(self.upper_case.findall(x)))
+
+        return float(upper_case_counts) / float(char_counts)
 
