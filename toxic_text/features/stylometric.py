f5efa98234211facc26abd41dbb38b083f677f25
ericrincon <eric.rincon147@gmail.com>
2018-03-08 23:45:15 -0600

    added character count

--- toxic_text/features/stylometric.py
+++ toxic_text/features/stylometric.py
@@ -7,6 +7,7 @@ class CharacterFeatures:
         exclamation_point_count: number of exclamation points in a string
         """
         self.exclamation_point_count = re.compile('\!')
+        self.char_count = re.compile('[^\s]]')
 
     def get_nb_exclamation_points(self, text):
         """
@@ -16,4 +17,12 @@ class CharacterFeatures:
         :return:
         """
         return text.apply(lambda x: len(self.exclamation_point_count.findall(x)))
+
+    def get_char_count(self, text):
+        """
+        Returns the number of characters in a string excluding whitespace
+        :param text: a pandas series of strings
+        :return: number of characters in each string
+        """
+        return text.apply(lambda x: len(self.char_count.findall(x)))
 
