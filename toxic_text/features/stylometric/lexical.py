1ee707b1548a7f53fdfb78809cb912849e5a4fe0
ericrincon <eric.rincon147@gmail.com>
2018-03-16 23:52:38 -0500

    Changed API for char level features to match word level features

--- toxic_text/features/stylometric/lexical.py
+++ toxic_text/features/stylometric/lexical.py
@@ -2,17 +2,18 @@ import re
 
 
 class CharacterFeatures:
-    def __init__(self):
+    def __init__(self, documents):
         """
         exclamation_point_count: number of exclamation points in a string
         """
+        self.documents = documents
         self.exclamation_point_count = re.compile('\!')
         self.char_count = re.compile('[^\s]')
         self.upper_case = re.compile('[A-Z]')
 
 
 
-    def get_nb_exclamation_points(self, text):
+    def get_nb_exclamation_points(self):
         """
         Returns the number of "!" in a comment
 
@@ -20,18 +21,18 @@ class CharacterFeatures:
         :return: pandas series where each element is the numebr of exclamation
         points in that element
         """
-        return text.apply(lambda x: len(self.exclamation_point_count.findall(x)))
+        return self.documents.apply(lambda x: len(self.exclamation_point_count.findall(x)))
 
-    def get_char_count(self, text):
+    def get_char_count(self):
         """
         Returns the number of characters in a string excluding whitespace
         :param text: a pandas series of strings
         :return: pandas series where each element is
         the number of characters in that string
         """
-        return text.apply(lambda x: len(self.char_count.findall(x)))
+        return self.documents.apply(lambda x: len(self.char_count.findall(x)))
 
-    def upper_case_ratio(self, text):
+    def upper_case_ratio(self):
         """
         Get the number of upper case characters over the total number
         of characters excluding whitespace
@@ -40,8 +41,8 @@ class CharacterFeatures:
         the ratio of uppercase over total counts
         """
 
-        char_counts = self.get_char_count(text)
-        upper_case_counts = text.apply(lambda x: len(self.upper_case.findall(x)))
+        char_counts = self.get_char_count()
+        upper_case_counts = self.documents.apply(lambda x: len(self.upper_case.findall(x)))
 
         return upper_case_counts.astype(float) / char_counts.astype(float)
 
